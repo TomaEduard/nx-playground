e9cd4ba4c2f7574a1a985e0fce0209aba3a3af5e
Eduard <eduard.daniel.toma@gmail.com>
2024-08-23 03:17:34 +0300

    😊

--- apps/shell-with-remotes/src/app/app.component.ts
+++ apps/shell-with-remotes/src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { SharedUtilsComponent } from '@nx-playground/shared-utils';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, SharedUtilsComponent],
+  imports: [NxWelcomeComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'my-app-3';
+  title = 'shell-with-remotes';
 }
